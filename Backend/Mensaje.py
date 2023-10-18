b9d8648185d1bd40548db0d28ccf7f2a7a3f490a
RoniRoland <roni0293@gmail.com>
2023-10-18 16:23:50 -0600

    clase mensaje

---
+++ Backend/Mensaje.py
@@ -0,0 +1,14 @@
+import re
+
+
+class Mensajes:
+    def __init__(self, fecha, texto) -> None:
+        self.fecha = fecha
+        self.texto = texto
+
+    def obtener_hashtags(self):
+        return re.findall(r"#\w+", self.texto)
+
+    def resetear(self):
+        self.fecha = ""
+        self.texto = ""
