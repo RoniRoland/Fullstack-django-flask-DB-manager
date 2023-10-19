2096042768b40e92f0bf872e2f1ed18dad316ffa
RoniRoland <roni0293@gmail.com>
2023-10-19 13:59:53 -0600

    id unico y funcion mensajes iguales

--- Backend/Mensaje.py
+++ Backend/Mensaje.py
@@ -2,9 +2,19 @@ import re
 
 
 class Mensajes:
+    contador_id = 1
+
     def __init__(self, fecha, texto) -> None:
         self.fecha = fecha
         self.texto = texto
+        self.id = Mensajes.contador_id
+        Mensajes.contador_id += 1
+
+    def __eq__(self, other):
+        return self.fecha == other.fecha and self.texto == other.texto
+
+    def obtener_texto(self):
+        return self.texto
 
     def obtener_hashtags(self):
         return re.findall(r"#\w+", self.texto)
